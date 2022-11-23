c40ce60b97c3ae3ebcd83d443ff5e5181afd53d6
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-11-23 17:04:15 -0300

    Gerando serviço do agente de obesidade

---
+++ obesidade/src/main/java/br/com/ifes/com/smarc/obesidade/SmarcAgenteDeObesidadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifes.com.smarc.obesidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarcAgenteDeObesidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarcAgenteDeObesidadeApplication.class, args);
+	}
+
+}
