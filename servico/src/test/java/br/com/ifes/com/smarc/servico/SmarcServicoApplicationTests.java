3dd9d2e9c9da4c2cbacd260632cd15f29ade594b
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-11-23 16:41:31 -0300

    Gerando projeto inicial

---
+++ servico/src/test/java/br/com/ifes/com/smarc/servico/SmarcServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifes.com.smarc.servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarcServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
