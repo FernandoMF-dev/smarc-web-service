f595d1ab91b09b34b170ec684a301edf720cc322
Mateus Padovan Pereira <mateuspadovanpereira@hotmail.com>
2022-11-24 20:15:25 -0300

    Adicionado banner personalizado ao serviço Agente de Pressão Arterial

---
+++ pressao/src/test/java/br/com/ifes/com/smarc/pressao/SmarcAgenteDePressaoArterialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifes.com.smarc.pressao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarcAgenteDePressaoArterialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
