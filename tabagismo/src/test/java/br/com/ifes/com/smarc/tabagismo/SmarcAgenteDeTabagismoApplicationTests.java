4dbb15752219a953421f52c5d2aa2029944dfdb8
Mateus Padovan Pereira <mateuspadovanpereira@hotmail.com>
2022-11-24 18:58:08 -0300

    Adicionado banner personalizado ao serviço Agente de Tabagismo

---
+++ tabagismo/src/test/java/br/com/ifes/com/smarc/tabagismo/SmarcAgenteDeTabagismoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifes.com.smarc.tabagismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarcAgenteDeTabagismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
