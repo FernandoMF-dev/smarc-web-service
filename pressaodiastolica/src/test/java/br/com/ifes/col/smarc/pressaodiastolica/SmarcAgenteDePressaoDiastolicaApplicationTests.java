f152c08ff4e0697dcd51ca75a54ae0f0c220dd4d
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-11-25 09:17:39 -0300

    Gerando serviço do agente de pressão diastólica

---
+++ pressaodiastolica/src/test/java/br/com/ifes/col/smarc/pressaodiastolica/SmarcAgenteDePressaoDiastolicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifes.col.smarc.pressaodiastolica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarcAgenteDePressaoDiastolicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
