28a79332b058d58490a5cf755b4ecf7b2a1d4b2d
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-11-25 08:42:31 -0300

    Aprimorando cálculo do risco de sedentarismo

--- sedentarismo/src/main/java/br/com/ifes/com/smarc/sedentarismo/service/SedentarismoService.java
+++ sedentarismo/src/main/java/br/com/ifes/com/smarc/sedentarismo/service/SedentarismoService.java
@@ -12,21 +12,11 @@ public class SedentarismoService {
 
 	public AgenteSedentarismo calcularResultado(AgenteSedentarismo agenteSedentarismo) {
 		switch (agenteSedentarismo.getFrequenciaAtividadeFisicaSemana()) {
-			case 0:
-				agenteSedentarismo.setResultado(1.0);
-				break;
-			case 1:
-				agenteSedentarismo.setResultado(0.75);
-				break;
-			case 2:
-				agenteSedentarismo.setResultado(0.5);
-				break;
-			case 3:
-				agenteSedentarismo.setResultado(0.25);
-				break;
-			default:
-				agenteSedentarismo.setResultado(0.0);
-				break;
+			case 0 -> agenteSedentarismo.setResultado(1.0);
+			case 1 -> agenteSedentarismo.setResultado(0.75);
+			case 2 -> agenteSedentarismo.setResultado(0.5);
+			case 3 -> agenteSedentarismo.setResultado(0.25);
+			default -> agenteSedentarismo.setResultado(0.0);
 		}
 		return agenteSedentarismo;
 	}
