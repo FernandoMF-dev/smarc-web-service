99007755987a6d3340235c99051581022ebbe821
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2022-11-25 08:58:17 -0300

    Aprimorando cálculo do risco de obesidade

--- obesidade/src/main/java/br/com/ifes/com/smarc/obesidade/service/ObesidadeService.java
+++ obesidade/src/main/java/br/com/ifes/com/smarc/obesidade/service/ObesidadeService.java
@@ -1,6 +1,7 @@
 package br.com.ifes.com.smarc.obesidade.service;
 
 import br.com.ifes.com.smarc.obesidade.domain.entities.AgenteObesidade;
+import br.com.ifes.com.smarc.obesidade.utils.NumberUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,19 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ObesidadeService {
 
+	private static final Double IMC_MIN = 25.0;
+	private static final Double IMC_MAX = 40.0;
+
 	public AgenteObesidade calcularResultado(AgenteObesidade agenteObesidade) {
 		calcularImc(agenteObesidade);
-		if (agenteObesidade.getImc() <= 25) {
-			agenteObesidade.setResultado(0.0);
-			return agenteObesidade;
-		}
-
-		if (agenteObesidade.getImc() >= 40) {
-			agenteObesidade.setResultado(1.0);
-			return agenteObesidade;
-		}
-
-		agenteObesidade.setResultado((agenteObesidade.getImc() - 25) / (40 - 25));
+		Double resultado = (agenteObesidade.getImc() - IMC_MIN) / (IMC_MAX - IMC_MIN);
+		agenteObesidade.setResultado(NumberUtils.limitToInterval(resultado, 0.0, 1.0));
 		return agenteObesidade;
 	}
 
---
+++ obesidade/src/main/java/br/com/ifes/com/smarc/obesidade/utils/NumberUtils.java
@@ -0,0 +1,12 @@
+package br.com.ifes.com.smarc.obesidade.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class NumberUtils {
+
+	public static Double limitToInterval(Double value, Double min, Double max) {
+		return Math.min(Math.max(value, min), max);
+	}
+
+}
